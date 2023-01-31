0ab95a5bbb9c5318a50ba4ebf4ac1d43cbc38663
Alberto2807 <123153884+Alberto2807@users.noreply.github.com>
2023-01-31 12:57:22 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,13 @@
+input.onButtonPressed(Button.A, function () {
+    contador += 1
+})
+input.onButtonPressed(Button.B, function () {
+    if (contador >= 0) {
+        contador += -1
+    }
+})
+let contador = 0
+contador = 0
 basic.forever(function () {
-	
+    basic.showNumber(contador)
 })
